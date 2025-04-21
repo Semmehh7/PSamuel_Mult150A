b698145ea8207d33a5838ab18b165c2a31a3b631
Semmehh7 <7samperry@gmail.com>
2025-04-20 21:52:24 -0700

    Hour8

---
+++ Sperry_Hour8_P1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int healthpoints = 3992;
+        healthpoints = UsePotion(healthpoints);
+        print(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+        print(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+        print(healthpoints);
+        healthpoints = UsePotion(healthpoints);
+        print(healthpoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int UsePotion(int health)
+    {
+        return health + 400;
+    }
+}
