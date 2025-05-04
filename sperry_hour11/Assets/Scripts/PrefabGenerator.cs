570ea762ae735407b413d8bba176476e73379fde
Semmehh7 <7samperry@gmail.com>
2025-05-04 14:47:50 -0700

    Hour11

---
+++ sperry_hour11/Assets/Scripts/PrefabGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public float SpawnSpacing = 0;
+
+    int counter = 0;
+
+    void Update()
+    {
+        // Whenever we hit the B key we will generate a prefab at the
+        // position of the original prefab 
+        // Whenever we hit the space key, we will generate a prefab at the
+        // position of the spawn object that this script is attached to
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position,transform.rotation);
+        }
+    }
+    void Start()
+    {
+        Vector3 pos = transform.position;
+
+        for (counter = 0; counter < 10; counter++)
+        {
+            print((counter) + (" spawning..."));
+            Instantiate(prefab, pos, transform.rotation);
+            pos.x += SpawnSpacing;
+        }
+    }
+}
