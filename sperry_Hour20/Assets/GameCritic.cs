f8eff5c816d70154ffc26abd3483e47d99327832
Semmehh7 <7samperry@gmail.com>
2025-05-26 13:54:05 -0700

    Fixed name of project

---
+++ sperry_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,20 @@
+//Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+//I would say the game is maybe a little too easy, obstacles move too slowly, and I feel like the power ups can be grabbed from too far away.
+
+//What 2 changes would you do to make it easier or harder?
+//Make the collider on the power ups a little smaller, and increase the movement speed of all obstales. Increasing the speed of obstacles overtime would be cool aswell.
+
+//List 2 things that would give it a "wow" factor.
+//Adding a new obstacle the player must jump over, adding a jump should not be too hard. Maybe adding a slow motion power up.
+
+//What 2 parts of the game are fun? 
+//The core loop of doging and picking up powerups is fun, the visuals of the powerups are also nice to look at.
+
+//What 2 parts of the game are tedious?
+//The game can get boring quick, not much reason to want to keep going as nothing changes. The obsticles are boring also compared to the powerups.
+
+
+//CHANGES
+
+//Added spikes to the top of the red obstacles
+//Made the powerups corgis because why not.
