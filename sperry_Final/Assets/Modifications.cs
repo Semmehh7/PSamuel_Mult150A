626027bd9fc9da3a745446f3d09da99f6be7c66c
Semmehh7 <7samperry@gmail.com>
2025-06-01 10:42:13 -0700

    Final

---
+++ sperry_Final/Assets/Modifications.cs
@@ -0,0 +1,10 @@
+// 1.Changed Directional Light to Green and doubled its intensity.
+// 2. Added black dark green frog with 0.06 Density
+// 3. Changed Atmsphere Thickness to 0.3 Changed Sky Tint to Green, Changed Ground to very dark green, Changed exposure to 0.9
+// 4. Added more spikes to model
+// 5. Organized Cones as children of obsticle.
+// 6. New fire and lava materials applied.
+// 7. New smoke particle system. Size over time changed, Color over time changed, Velocity and start speed changed.
+// 8. Doubled Light intensity on powerup.
+// 9. created wall prefab.
+// 10. Created blue wall materiall called wall 2.
